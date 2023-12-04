27cfd3a700c6305fbb5003f5a6e20ca65cf57a93
Ferran Guillén <fguillen@newrelic.com>
2023-12-04 08:45:20 +0100

    route basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
