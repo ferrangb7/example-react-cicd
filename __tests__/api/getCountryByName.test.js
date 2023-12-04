be9851cc9c4d3404782a658de3f6ee0533764e39
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-12-04 22:56:37 +0100

    Update getCountry info to contain mock test

--- __tests__/api/getCountryByName.test.js
+++ __tests__/api/getCountryByName.test.js
@@ -1,3 +1,6 @@
 //TODO: write a mock to simulate data being fetched from the API
 //Use this to help: https://dev.to/zaklaughton/the-only-3-steps-you-need-to-mock-an-api-call-in-jest-39mb
 
+test('getCountryByName returns expected country', async () => {
+    expect("REPLACE_ME").toBe("REPLACE_ME");
+});
