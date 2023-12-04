09bd7a25e444edd90c6a5e1d12469ac9782c7176
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-12-04 22:13:11 +0100

    Add snapshot test config

---
+++ __tests__/components/Search.test.js
@@ -0,0 +1,10 @@
+//Look at this doc to help you write the test: https://jestjs.io/docs/snapshot-testing
+
+//TODO: write a snapshot test that captures the Search.js component
+
+import renderer from 'react-test-renderer';
+import { Search } from '../../src/components/Search';
+
+test('Banner renders correctly', () => {
+    expect("REPLACE_ME").toBe("REPLACE_ME");
+});
