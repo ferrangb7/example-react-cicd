fbdaa745fa76cea5008be6b78556211c643953a7
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-12-04 22:03:41 +0100

    Add website info for getInfo.js

--- __tests__/helpers/getInfo.test.js
+++ __tests__/helpers/getInfo.test.js
@@ -1,3 +1,5 @@
+//More information can be found here: https://jestjs.io/docs/getting-started
+
 //TODO: write a test that checks that the names are joining correctly
 
 //TODO: write a test if no data is passed to the function (returns "none")
