a32f487bc4f9fb57fcb4c54f49299932a54d3755
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-12-04 22:02:50 +0100

    Add holding test for getInfo.js

---
+++ __tests__/helpers/getInfo.test.js
@@ -0,0 +1,7 @@
+//TODO: write a test that checks that the names are joining correctly
+
+//TODO: write a test if no data is passed to the function (returns "none")
+
+test('REPLACE_ME', () => {
+    expect("REPLACE_ME").toBe("REPLACE_ME");
+});
