240a1a848a01e7f6ea934d6dd18272303fd71341
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-12-04 22:51:37 +0100

    Add DOM test info

--- __tests__/components/Search.test.js
+++ __tests__/components/Search.test.js
@@ -8,3 +8,17 @@ import { Search } from '../../src/components/Search';
 test('Search box renders correctly', () => {
     expect("REPLACE_ME").toBe("REPLACE_ME");
 });
+
+//TODO: write a test that checks that the search field has some text typed in
+
+//This is a hard one! You'll need to use Queries to find the elements you're looking for:
+// https://testing-library.com/docs/queries/about/
+// And you'll need to simulate someone clicking on the button:
+// https://testing-library.com/docs/dom-testing-library/api-events
+
+//Start here: https://testing-library.com/docs/example-findByText
+//Look for the line that says: 'describe('findByText Examples', () => {'
+
+test('when a user types in the search bar, the search bar contains text', async () => {
+    expect("REPLACE_ME").toBe("REPLACE_ME");
+});
