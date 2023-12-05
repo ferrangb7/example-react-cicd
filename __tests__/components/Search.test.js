1bacbd409a9c124e61d5b40f076ccfce147da4eb
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-12-05 08:29:00 +0100

    Format tests

--- __tests__/components/Search.test.js
+++ __tests__/components/Search.test.js
@@ -1,19 +1,11 @@
-//Look at this doc to help you write the test: https://jestjs.io/docs/snapshot-testing
-
-//TODO: write a snapshot test that captures the Search.js component
-
 import renderer from 'react-test-renderer'; //you will need this for the snapshot test
 import { cleanup, fireEvent, render } from '@testing-library/react'; //this is for the DOM testing
 import { screen } from '@testing-library/dom';//this is for the DOM testing
 import { Search } from '../../src/components/Search';
 
-test('Search box renders correctly', () => {
-    expect("REPLACE_ME").toBe("REPLACE_ME");
-});
-
 //TODO: write a test that checks that the search field has some text typed in
 
-//This is a hard one! You'll need to use Queries to find the elements you're looking for:
+//You'll need to use Queries to find the elements you're looking for:
 // https://testing-library.com/docs/queries/about/
 // And you'll need to simulate someone clicking on the button:
 // https://testing-library.com/docs/dom-testing-library/api-events
@@ -21,14 +13,25 @@ test('Search box renders correctly', () => {
 //Start here: https://testing-library.com/docs/example-findByText
 //Look for the line that says: 'describe('findByText Examples', () => {'
 
-test('when a user types in the search bar, the search bar contains text', async () => {
+test('REPLACE_ME', async () => {
     expect("REPLACE_ME").toBe("REPLACE_ME");
 });
 
-//TODO: write a test to check that the Error component appears if no data 
-//is found from the call to the API. You will need another mock.
-//THIS IS THE HARDEST -- try the getCountryByName.test.js first!
+//TODO: write a snapshot test that captures the Search.js component
 
-test('when no countries are found, an error message appears', async () => {
+//Look at this doc to help you write the test: https://jestjs.io/docs/snapshot-testing
+
+test('Search box renders correctly', () => {
+    expect("REPLACE_ME").toBe("REPLACE_ME");
+});
+
+
+//TODO: write a test to check that the Error component appears if no data 
+
+//is found from the call to the API. You will need another mock.
+
+//This is is a hard test to write - try the getCountryByName.test.js test first!
+
+test('REPLACE_ME', async () => {
     expect("REPLACE_ME").toBe("REPLACE_ME");
 });
--- __tests__/helpers/getInfo.test.js
+++ __tests__/helpers/getInfo.test.js
@@ -1,6 +1,10 @@
-//More information can be found here: https://jestjs.io/docs/getting-started
-
 //TODO: write a test that checks that the names are joining correctly
+//More information can be found here: https://jestjs.io/docs/getting-started
+//You might want to have a go at using some of Jest's other matchers: https://jestjs.io/docs/expect
+
+test('REPLACE_ME', () => {
+    expect("REPLACE_ME").toBe("REPLACE_ME");
+});
 
 //TODO: write a test if no data is passed to the function (returns "none")
 
