203047b42304f02662c294764e16d7cf341d0afe
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-06-11 20:11:10 +0200

    remove one challenge and add note to see it failing

--- __tests__/components/Search.test.js
+++ __tests__/components/Search.test.js
@@ -8,28 +8,9 @@ import { Search } from '../../src/components/Search';
 to make sure that nothing gets moved around or changed unexpectedly.
 */
 
-//TODO: write a snapshot test that checks that the Search.js component actually renders
+//TODO: write a snapshot test that checks that the Search.js component actually renders, then change something to see the test fail
 //Look at this doc to help you write the test: https://jestjs.io/docs/snapshot-testing
 
 test('REPLACE_ME', () => {
     expect("REPLACE_ME").toBe("REPLACE_ME");
 });
-
-//TODO: write a test that checks that the search field in the component has some text typed in
-
-test('REPLACE_ME', async () => {
-    expect("REPLACE_ME").toBe("REPLACE_ME");
-});
-
-/*
-Here are some documents to help you get started:
-
-You'll need to use Queries to find the elements you're looking for:
-https://testing-library.com/docs/queries/about/
-
-You'll need to simulate someone typing the text in:
-https://testing-library.com/docs/dom-testing-library/api-events
-
-Start here: https://testing-library.com/docs/example-findByText
-Look for the line that says: 'describe('findByText Examples', () => {'
-*/
