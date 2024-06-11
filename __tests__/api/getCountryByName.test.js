be9e3326ffea780d1b4a86209663b708b9b412fd
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2024-06-11 19:30:21 +0200

    add instructions for mock api test

--- __tests__/api/getCountryByName.test.js
+++ __tests__/api/getCountryByName.test.js
@@ -1,6 +1,30 @@
-//TODO: write a mock to simulate data being fetched from the API
-//Use this to help: https://dev.to/zaklaughton/the-only-3-steps-you-need-to-mock-an-api-call-in-jest-39mb
+/*
+5. This test suite is all about creating a mock to test third-party behaviours.
+
+TODO: write a mock to simulate data being fetched from the API
+Use this to help: https://dev.to/zaklaughton/the-only-3-steps-you-need-to-mock-an-api-call-in-jest-39mb
+*/
+
+import axios from 'axios'
+import { getCountryByName } from "../src/api/getCountryByName";
+
+jest.mock('axios');
 
 test('REPLACE_ME', async () => {
-    expect("REPLACE_ME").toBe("REPLACE_ME");
+  /* Arrange: mock result of axios.get method.
+
+  This should be similar to the object you're getting back from the API.
+   If you're stuck, have a look at the response from the API with devtools and have a look at the component to see
+  what it is expecting
+  */
+  axios.get.mockResolvedValue("REPLACE_ME");
+
+  // Act: use the component under test to get countries by name
+  const countries = await getCountryByName('REPLACE_ME');
+
+  // Assert: check that the result is the expected one
+  expect(countries.length).toEqual(1);
+  expect(countries[0].name.common).toEqual('REPLACE_ME');
 });
+
+//TODO: If you found that easy, try writing simulating the instance where a user writes 'United' and gets back multiple countries
